c84540915ada0f678e73217bdb8411c9fab15b44
Christophorus Parikesit Wistara <100826197+gangsi8n@users.noreply.github.com>
2022-03-12 16:31:02 +0700

    Menambakan class kodebuah

---
+++ jurnal_kpl_mod3/MOD3_1302203123/MOD3_1302203123/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MOD3_1302203123
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            buahbatu nama = new buahbatu();
+            nama.getKodeBuah();
+        }
+    }
+}
